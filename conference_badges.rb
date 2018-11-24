0ceb2c210a0e7f8ac665e58981123d744c1b42e9
Alexander Baker <alex.baker685@gmail.com>
2018-11-24 20:15:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,12 +4,12 @@ end
 
 def batch_badge_creator(list_of_names)
   badges = []
-  list_of_names.each {|name| badges << "Hello, my name is #{name}."}
+    list_of_names.each {|name| badges << "Hello, my name is #{name}."}
   badges
 end
 
 def assign_rooms(list_of_names)
   assignments = []
-  list_of_names.each_with_index {|name, room| assignments << "Hello, #{name}! You'll be assigned to room #{room}!" }
+    list_of_names.each_with_index {|name, room| assignments << "Hello, #{name}! You'll be assigned to room #{room}!" }
   assignments
 end
